f5a267516ba15c86a8d4b62baa81bb6111375831
Omelchenko Michael <l2maximum@mail.ru>
2020-02-10 15:16:27 +0300

    урок 18, add ajax filters movies

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.views.generic.base import View
@@ -57,3 +58,17 @@ class FilterMoviesView(GenreYear, ListView):
             Q(genres__in=self.request.GET.getlist("genre"))
         )
         return queryset
+
+
+class JsonFilterMoviesView(ListView):
+    """Фильтр фильмов в json"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        ).distinct().values("title", "tagline", "url", "poster")
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        queryset = list(self.get_queryset())
+        return JsonResponse({"movies": queryset}, safe=False)
---
+++ static/js/scripts.js
@@ -0,0 +1,59 @@
+function ajaxSend(url, params) {
+    // Отправляем запрос
+    fetch(`${url}?${params}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    })
+        .then(response => response.json())
+        .then(json => render(json))
+        .catch(error => console.error(error))
+}
+
+const forms = document.querySelector('form[name=filter]');
+
+forms.addEventListener('submit', function (e) {
+    // Получаем данные из формы
+    e.preventDefault();
+    let url = this.action;
+    let params = new URLSearchParams(new FormData(this)).toString();
+    ajaxSend(url, params);
+});
+
+function render(data) {
+    // Рендер шаблона
+    let template = Hogan.compile(html);
+    let output = template.render(data);
+
+    const div = document.querySelector('.left-ads-display>.row');
+    div.innerHTML = output;
+}
+
+let html = '\
+{{#movies}}\
+    <div class="col-md-4 product-men">\
+        <div class="product-shoe-info editContent text-center mt-lg-4">\
+            <div class="men-thumb-item">\
+                <img src="media/{{ poster }}" class="img-fluid" alt="">\
+            </div>\
+            <div class="item-info-product">\
+                <h4 class="">\
+                    <a href="/{{ url }}" class="editContent">{{ title }}</a>\
+                </h4>\
+                <div class="product_price">\
+                    <div class="grid-price">\
+                        <span class="money editContent">{{ tagline }}</span>\
+                    </div>\
+                </div>\
+                <ul class="stars">\
+                    <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>\
+                    <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>\
+                    <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>\
+                    <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>\
+                    <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>\
+                </ul>\
+            </div>\
+        </div>\
+    </div>\
+{{/movies}}'

4241c44af4c2170ab159f88cb541f668086e45fd
DJWOMS <socanime@gmail.com>
2020-03-03 13:52:08 +0300

    урок 21, add search

--- movies/urls.py
+++ movies/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.MoviesView.as_view()),
     path("filter/", views.FilterMoviesView.as_view(), name='filter'),
+    path("search/", views.Search.as_view(), name='search'),
     path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
     path("json-filter/", views.JsonFilterMoviesView.as_view(), name='json_filter'),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
--- movies/views.py
+++ movies/views.py
@@ -114,3 +114,16 @@ class AddStarRating(View):
             return HttpResponse(status=201)
         else:
             return HttpResponse(status=400)
+
+
+class Search(ListView):
+    """Поиск фильмов"""
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Movie.objects.filter(title__icontains=self.request.GET.get("q"))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["q"] = f'q={self.request.GET.get("q")}&'
+        return context

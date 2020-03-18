d4a938c436675e3a0c7909f36e105d59abd9a31c
DJWOMS <socanime@gmail.com>
2020-03-18 12:24:22 +0300

    урок 26, add allauth

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'movies',
     'contact',
     'snowpenguin.django.recaptcha3',
+
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
@@ -90,7 +93,10 @@ DATABASES = {
     }
 }
 
-
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -122,6 +128,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_REDIRECT_URL = "/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 
 gettext = lambda s: s
--- django_movie/urls.py
+++ django_movie/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
 ]
 
 urlpatterns += i18n_patterns(
+    path('accounts/', include('allauth.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('contact/', include("contact.urls")),
     path("", include("movies.urls")),
